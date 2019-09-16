b66e7fa27ce4bfc4ac88c2ac36ee4c2eee01044c
Darin Pope <darin@planetpope.com>
2019-09-16 17:40:40 -0400

    Update notification.groovy

--- notification.groovy
+++ notification.groovy
@@ -5,8 +5,13 @@ import com.cloudbees.jenkins.plugins.notification.api.NotificationConfiguration
 import com.cloudbees.jenkins.plugins.notification.spi.Router
 import com.cloudbees.opscenter.plugins.notification.OperationsCenterRouter
 
+def instance = Jenkins.getInstance();
+
 Router r = new OperationsCenterRouter();
+
 NotificationConfiguration config = ExtensionList.lookupSingleton(NotificationConfiguration.class);
 config.setRouter(r);
 config.setEnabled(true);
 config.save();
+
+instance.save();
