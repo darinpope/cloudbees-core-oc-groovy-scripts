0a4a1a2eba7494c8e6c9191811e07da7be9a7b7e
Darin Pope <darin@planetpope.com>
2019-09-17 15:15:05 -0400

    Update uninstall-plugin.groovy

--- uninstall-plugin.groovy
+++ uninstall-plugin.groovy
@@ -4,7 +4,7 @@ import hudson.*;
 import hudson.model.*;
 
 def pluginManager = Jenkins.instance.getPluginManager();
-def pluginWrapperToUninstall = pluginManager.getPlugin(${PLUGIN_TO_REMOVE});
+def pluginWrapperToUninstall = pluginManager.getPlugin("${PLUGIN_TO_REMOVE}");
 if(pluginWrapperToUninstall != null) {
   pluginWrapperToUninstall.doDoUninstall();
   Jenkins.instance.doQuietDown(true,0);
